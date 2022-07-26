4a5fff8c26b8478ce50cf59d947e041b89d3881e
kkt <670335957@qq.com>
2022-07-26 11:52:07 +0800

    try

--- midassessment/src/routes/index.js
+++ midassessment/src/routes/index.js
@@ -1,10 +1,16 @@
-import Home from '../pages/Home/index'
 import Contents from '../pages/Home/Contents/index'
 import '../pages/Home/index.css'
+import { Navigate } from 'react-router-dom'
 
 export default [
     {
-        path:'/home',
-        element:<Home/>
+        // 主页子导航栏内容
+        path:'/Contents',
+        element:<Contents/>
+    },
+    {
+        // 主页子导航栏没有点击路由链接之前
+        path:'/',
+        element:<Navigate to="Contents"/>
     }
 ]
